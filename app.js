2587aa1d28c6d0771d596435c07d5d9ea5511671
GoLivefast <ProjectGoLive@mrwin.win>
2023-04-16 19:38:40 +0200

    basic functionality:the template ejs works

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-test;
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.set("view engine", "ejs");
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+app.get("/", (req,res) => {
+  res.render("home", { name: "something" });
+});
