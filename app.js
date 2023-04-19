58b848024f62a635cb70dd2b5d591ce1617ed84b
joe <joe@joe.joe>
2023-04-19 19:01:25 +0000

    can connect to mogodb online and create string inside a collection

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+require("dotenv").config();
+//<< mongoose part
+const mongoose = require("mongoose");
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URI);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const ralph = new Kitten({
+  name:"ralphas4"
+});
+
+ralph.save();
+
+//mangoose part>>
 const port = process.env.PORT || 5000;
 app.set("view engine", "ejs");
 app.listen(port, () => {
